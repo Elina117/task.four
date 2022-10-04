27360b7044e624ebbfa8981fda584b128ecef99b
Elina117 <106472257+Elina117@users.noreply.github.com>
2022-10-04 17:27:19 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Data;
+
+
+namespace SquareRoot
+{
+    public class SquareRootCalculator
+    {
+
+        static void Main(string[] args)
+        {
+
+            double a, b, c;
+            Console.WriteLine("Введите a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            double[] result = solveSquareRoot(a, b, c);
+
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+        }
+
+        public static double[] solveSquareRoot(double a, double b, double c)
+        {
+            double D;
+
+            D = Math.Pow(b, 2) - 4 * a * c;
+            if (D > 0)
+            {
+                double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                double[] expected = {x1, x2};
+                return expected;
+
+            }
+            else if (D==0)
+            {
+                double x1 = -b / (2 * a);
+                double[] expected = { x1 };
+                return expected;
+            }
+
+            else
+            {
+                double[] expected = { };
+                return expected;
+                
+            }
+        }
+            
+       
+
+    }
+     
+    
+}
---
+++ task4test/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using SquareRoot;
+
+namespace task4test;
+
+
+[TestClass]
+public class Task4Test
+{
+    
+    [TestMethod]
+    public void TestMethod1()
+    {
+        double a = 1;
+        double b = 6;
+        double c = 5;
+
+        double[] expected = { -1, -5 };
+
+        double[] actual = SquareRootCalculator.solveSquareRoot(a, b, c);
+
+        CollectionAssert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        double a = 1;
+        double b = 2;
+        double c = 1;
+
+        double[] expected = { -1 };
+
+        double[] actual = SquareRootCalculator.solveSquareRoot(a, b, c);
+
+        CollectionAssert.AreEqual(expected, actual);
+    }
+    [TestMethod]
+    public void TestMethod3()
+    {
+        double a = 1;
+        double b = 0;
+        double c = -4;
+
+        double[] expected = { 2, -2 };
+
+        double[] actual = SquareRootCalculator.solveSquareRoot(a, b, c);
+
+        CollectionAssert.AreEqual(expected, actual);
+    }
+    [TestMethod]
+    public void TestMethod4()
+    {
+        double a = 1;
+        double b = 4;
+        double c = 4;
+
+        double[] expected = { -2 };
+
+        double[] actual = SquareRootCalculator.solveSquareRoot(a, b, c);
+
+        CollectionAssert.AreEqual(expected, actual);
+    }
+    [TestMethod]
+    public void TestMethod5()
+    {
+        double a = 5;
+        double b = 1;
+        double c = 4;
+
+        double[] expected = { };
+
+        double[] actual = SquareRootCalculator.solveSquareRoot(a, b, c);
+
+        CollectionAssert.AreEqual(expected, actual);
+    }
+    [TestMethod]
+    public void TestMethod6()
+    {
+        double a = 1;
+        double b = 3;
+        double c = -70;
+
+        double[] expected = { 7, -10};
+
+        double[] actual = SquareRootCalculator.solveSquareRoot(a, b, c);
+
+        CollectionAssert.AreEqual(expected, actual);
+    }
+}
